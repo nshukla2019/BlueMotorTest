a7ea3986d79cc76fa31f651fecce8b30a5f51b1d
nshukla2019 <nupi.shukla@gmail.com>
2021-09-20 18:02:42 -0400

    working code!

--- src/main.cpp
+++ src/main.cpp
@@ -5,14 +5,14 @@
 BlueMotor blueMotor;
 Button bootButton(BOOT_FLAG_PIN);
 
-long timeToPrint = millis();
+long timeToPrint = 0;
 long now = 0;
 long newPosition = 0;
 long oldPosition = 0;
 long sampleTime = 100;
 int speedInRPM = 0;
 int CPR = 270;
-int motorEffort = 255;
+int motorEffort = -255;
 
 
 void setup() {
@@ -25,47 +25,32 @@ void setup() {
 
 void loop()
 {
+    timeToPrint = millis() + sampleTime;  
     oldPosition = blueMotor.getPosition();
    
-  //while (bootButton.CheckButtonPress()){
-    // The button is currently pressed.
+  while (1){
+   
     blueMotor.setEffort(motorEffort);
     
-    now = millis();
-    
-    if (now > timeToPrint)
+    if ((now = millis()) > timeToPrint) // set now to whatever time it is at this point and compare to timetoPrint
     {
-      timeToPrint = millis() + sampleTime;
+      timeToPrint = now + sampleTime;
       newPosition = blueMotor.getPosition();
  
-      Serial.print("new pos      ");
-      Serial.print(newPosition);
-      Serial.print("      ");
-
-      Serial.print("old pos      ");
-      Serial.print(oldPosition);
-      Serial.print("      ");
-
-      long pos = newPosition-oldPosition;
-      Serial.print("pos      ");
-      Serial.println(pos);
-
       speedInRPM = ((newPosition-oldPosition)*1000*60)/(sampleTime*CPR);
 
-     
-
-      // Serial.println("new line");
-      // Serial.print(now);
-      // Serial.print("      ");
-      // Serial.print(newPosition);
-      // Serial.print("      ");
-      // Serial.println(speedInRPM);
+      Serial.println("new line");
+      Serial.print(now);
+      Serial.print("      ");
+      Serial.print(newPosition);
+      Serial.print("      ");
+      Serial.println(speedInRPM);
       oldPosition = newPosition;
     }
     
-  //}
+  }
 
- //blueMotor.setEffort(0);
+ blueMotor.setEffort(0);
 
 
 }
